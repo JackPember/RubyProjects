bb1d48cd63e041bd66bf80768b730c97e51b5c1f
JackPember <jpember97@gmail.com>
2023-06-05 20:09:09 -0500

    added caesarChiper to ruby projects

---
+++ caesarCipher.rb
@@ -0,0 +1,25 @@
+require 'pry-byebug'
+def caesar_cipher(word, shift)
+    min_upper = 65
+    max_upper = 90
+    min_lower = 97
+    max_lower = 122
+    word = word.split("")
+    word.map! do |letter|
+        #binding.pry
+        if(letter.ord + shift).between?(min_upper, max_upper) || (letter.ord + shift).between?(min_lower, max_lower)
+            (letter.ord + shift).chr
+        elsif (letter.ord) < min_upper || (letter.ord) > max_lower
+            letter
+        elsif (letter.ord + shift) > max_upper || (letter.ord + shift) > max_lower
+            ((letter.ord + shift) - 26).chr
+        elsif (letter.ord + shift) < min_upper || (letter.ord + shift) < min_lower
+            ((letter.ord + shift) + 26).chr
+        end
+    end
+    puts word.join("")
+end
+    
+caesar_cipher("I fucking did it!!", 5)
+
+
